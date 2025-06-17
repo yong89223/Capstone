96019fa7e1a83e8c046b113bf70c725db66ace60
yong89223 <117891692+yong89223@users.noreply.github.com>
2025-06-18 01:28:41 +0900

    Update segmentation.py

--- finger_folder/utils/segmentation.py
+++ finger_folder/utils/segmentation.py
@@ -1,16 +1,4 @@
-"""
-In order to eliminate the edges of the image and areas that are too noisy, segmentation is
-necessary. It is based on the calculation of the variance of gray levels. For this purpose, the image
-is divided into sub-blocks of (W × W) size’s and for each block the variance.
-Then, the root of the variance of each block is compared with a threshold T, if the value obtained
-is lower than the threshold, then the corresponding block is considered as the background of the
-image and will be excluded by the subsequent processing.
 
-The selected threshold value is T = 0.2 and the selected block size is W = 16
-
-This step makes it possible to reduce the size of the useful part of the image and subsequently to
-optimize the extraction phase of the biometric data.
-"""
 import numpy as np
 import cv2 as cv
 
@@ -20,16 +8,7 @@ def normalise(img):
 
 
 def create_segmented_and_variance_images(im, w, threshold=.1):
-    """
-    Returns mask identifying the ROI. Calculates the standard deviation in each image block and threshold the ROI
-    It also normalises the intesity values of
-    the image so that the ridge regions have zero mean, unit standard
-    deviation.
-    :param im: Image
-    :param w: size of the block
-    :param threshold: std threshold
-    :return: segmented_image
-    """
+    
     (y, x) = im.shape
     threshold = np.std(im)*threshold
 
